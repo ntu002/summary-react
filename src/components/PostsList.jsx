90761cd8eab6d50e9ed4350a4ce4b29455ec5c86
ntu002 <ngoctubn2002@gmail.com>
2023-06-15 20:21:22 +0700

    add botton

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -3,23 +3,48 @@ import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
+import Modal from "./Modal";
 
-function PostsList() {
+function PostsList({isPosting, onStopPosting}) {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
 
-    const [ enteredBody, setEnteredBody ] = useState('');
-    const [ enteredAuthor, setEnteredAuthor ] = useState('');
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+  
+  //   let modalContent;
 
-    function bodyChangeHandler(event) {
-        setEnteredBody(event.target.value);
-      }
-      function authorChangeHandler(event) {
-        setEnteredAuthor(event.target.value);
-      }
-    
+  //   if (modalIsVisile) {
+  //     modalContent = (
+  //       <Modal onClose={hideModalHandler}>
+  //         <NewPost
+  //           onBodyChange={bodyChangeHandler}
+  //           onAuthorChange={authorChangeHandler}
+  //         />
+  //       </Modal>
+  //     );
+  //   }
 
   return (
     <>
-      <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+            onCancel={onStopPosting}
+          />
+        </Modal>
+      )}
+
+      {/* <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      /> */}
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Author 2" body="Text two" />
